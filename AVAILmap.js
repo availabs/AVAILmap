2c518f34c2786754c931917a7462a12f383a8f89
Eric Conklin <eric.s.conklin@gmail.com>
2014-04-29 14:46:49 -0400

    minor changes

--- AVAILmap.js
+++ AVAILmap.js
@@ -6,42 +6,10 @@
     // cache object constructor function
     function _LayerCache() {
     	var self = this,
-    		cache = [],
-    		choros = [];	// An array of objects with keys: attr, domain.
-
-    	// Parameter: pleth - a geoJSON property name. The property's domain should be numeric.
-    	self.addChoro = function(pleth) {
-    		for (i in choros) {
-    			if (choros[i].attr.toLowerCase() == pleth.toLowerCase())
-    				return
-    		}
-    		choros.push({attr: pleth, domain: [null, null]});
-    	}
+    		cache = [];
 
     	self.putJSON = function(json, id) {
     		cache.push(new _CacheObject(json, id));
-    		/*if (choros.length > 0) {
-    			_findDomains(json.features);
-    		}*/
-    	}
-
-    	function _findDomains(features) {
-    		for (i in choros) {
-    			_findDomain(features, choros[i]);
-    		}
-    	}
-
-    	function _findDomain(features, choro) {
-    		var min = choro.domain[0] || features[0][choro.attr],
-    			max = choro.domain[1] || features[0][choro.attr];
-    		for (i in features) {
-    			// check for new domain min
-    			if (features[i][choro.attr] < min)
-    				choro.domain[0] = features[i][choro.attr];
-    			// check for new domain max
-    			if (features[i][choro.attr] > max)
-    				choro.domain[1] = features[i][choro.attr];
-    		}
     	}
 
     	self.getJSON = function(id) {
@@ -79,7 +47,13 @@
     		layer = null,
     		baseURL = url,
     		drawFunc,
+    		zIndex,
+    		hover,			// false or stores a class name that is applied to polygons on mouseover
+    		IDtag,
+    		tilePath,
+    		map,
     		cache = new _LayerCache(),
+    		requests = [],	// Array to store XML HTTP Requests
     		properties,		// Array of geoJSON property names.
     						// Creates classes from specified geoJSON properties.
     						// Each class is named using the following scheme:
@@ -88,22 +62,30 @@
     		styles,			// Array of class names.
     						// Assigns the classes to all objects on the layer.
 
-    		choros;			// Array  of objects with keys: attr, range.
-    						// The attr key is a geoJSON property name.
-    						// The range key is an array of colors.
-    						// Creates a d3.js quantize scale with range of color,
-    						//		and domain [min, max] of the attr key
+    		choros;			// Not implemented
 
     	if (typeof options !== 'undefined') {
     		drawFunc = options.func || _drawTile;
     		properties = options.properties || [];
     		styles = options.styles || [];
     		choros = options.choros || [];
+    		zIndex = options.zIndex || 0;
+    		hover = options.hover || false;
     	} else {
     		drawFunc = _drawTile;
     		styles = [];
     		properties = [];
     		choros = [];
+    		zIndex = 0;
+    		hover = false;
+    	}
+
+    	self.getID = function() {
+    		return IDtag;
+    	}
+
+    	self.setID = function(id) {
+    		IDtag = id;
     	}
 
     	self.getURL = function() {
@@ -130,73 +112,142 @@
     		layer = l;
     	}
 
-    	self.getChoros = function() {
-    		return choros;
+    	self.setTilePath = function(tp) {
+    		tilePath = tp;
     	}
 
-    	self.addChoro = function(pleths) {
-    		for (i in pleths) {
-    			if (choros.indexOf(pleths[i] === -1))
-    				choros.push(pleths[i]);
+    	self.setMap = function(m) {
+    		map = m;
+    	}
+
+    	self.getHover =function() {
+    		return hover;
+    	}
+
+    	self.getZIndex = function() {
+    		return zIndex;
+    	}
+
+    	function _addAttributes(lst, atts) {
+    		var len = lst.length;
+    		for (i in atts) {
+    			if (lst.indexOf(atts[i] === -1))
+    				lst.push(atts[i]);
     		}
+    		if (len !== lst.length) {
+    			map.drawLayer(self);
+    		}
+    	}
+
+    	function _removeAttributes(lst, atts) {
+    		var len = lst.length;
+    		for (var i = atts.length-1; i >= 0; i--) {
+    			if (lst.indexOf(atts[i] !== -1))
+    				lst.splice(i, 1);
+    		}
+    		if (len !== lst.length) {
+    			map.drawLayer(self);
+    		}
+    	}
+
+    	self.getChoros = function() {
+    		console.log('Not implemented');
+    	}
+    	self.addChoros = function(pleths) {
+    		console.log('Not implemented');
+    	}
+    	self.removeChoros = function(pleths) {
+    		console.log('Not implemented');
     	}
 
     	self.getStyles = function() {
     		return styles;
     	}
-
-    	self.addStyle = function(cls) {
-    		for (i in cls) {
-    			if (styles.indexOf(cls[i] === -1))
-    				styles.push(cls[i]);
-    		}
+    	self.addStyles = function(cls) {
+    		_addAttributes(styles, cls);
+    	}
+    	self.removeStyles = function(cls) {
+    		_removeAttributes(styles, cls);
     	}
 
     	self.getProperties = function() {
     		return properties;
     	}
-
-    	self.addProperty = function(props) {
-    		for (i in props) {
-    			if (properties.indexOf(props[i] === -1))
-    				properties.push(props[i]);
-    		}
+    	self.addProperties = function(props) {
+    		_addAttributes(properties, props);
+    	}
+    	self.removeProperties = function(props) {
+    		_removeAttributes(properties, props);
     	}
 
-    	var requests = [];
-
-    	self.abortXHR = function(el) {
-    		for (i in requests) {
-    			if (requests[i] === el) {
-    				el._xhr.abort();
+    	self.abortXHR = function(d) {
+    		var id = _generateTileID(d);
+    		for (var i = requests.length-1; i >= 0; i--) {
+    			if (requests[i].getTag() === id) {
+    				requests[i].abort();
     				requests.splice(i, 1);
-    				return;
+    				break;
     			}
     		}
     	}
 
-    	self.drawTile = function(SVG, d, tilePath) {
-    		var id = 'tile-' + d.join('-');
+    	function _removeXHR(xhr) {
+    		for (i in requests) {
+    			if (requests[i] === xhr) {
+    				requests.splice(i, 1);
+    				break;
+    			}
+    		}
+    	}
+
+    	self.drawTile = function(SVG, d) {
+    		var id = _generateTileID(d);
     		var json = cache.getJSON(id);
     		if (json === null) {
-    			requests.push(SVG);
-    			var URL = makeURL(d, baseURL);
-	  			SVG._xhr = d3.json(URL, function(error, data) {
-    				drawFunc(SVG, d, tilePath, self, data);
+    			var xhr = new _Request(id),
+    				URL = _makeURL(d, baseURL);
+    			requests.push(xhr);
+	  			xhr.setXHR(d3.json(URL, function(error, data) {
+    				_removeXHR(xhr);
     				cache.putJSON(data, id);
-	  			});
+    				drawFunc(SVG, d, tilePath, self, data);
+	  			}));
     		} else {
     			drawFunc(SVG, d, tilePath, self, json);
     		}
     	}
 
-	    function makeURL(d, url) {
+	    function _makeURL(d, url) {
 			if (url.indexOf('http://tile.openstreetmap') === 0) {
 				url = url.replace('http://', 'http://' + ["a", "b", "c"][(d[0] * 31 + d[1]) % 3] + '.')
 			}
 			url += "/" + d[2] + "/" + d[0] + "/" + d[1] + ".topojson";
 			return url;
 	    }
+
+	    // XHR object constructor
+	    function _Request(tag) {
+	    	var self = this,
+				_tag = tag,
+	    		_xhr = null;
+
+	    	self.getTag = function() {
+	    		return _tag;
+	    	}
+	    	self.getXHR = function() {
+	    		return _xhr;
+	    	}
+	    	self.setXHR = function(xhr) {
+	    		_xhr = xhr;
+	    	}
+	    	self.abort = function() {
+	    		_xhr.abort();
+	    	}
+	    }
+    }
+
+    function _generateTileID(d) {
+    	return 'tile-' + d.join('-');
     }
 
     function _drawTile(SVG, d, tilePath, layer, json) {
@@ -205,115 +256,54 @@
 
 	  	var svg = d3.select(SVG);
 
+	  	var pathLayerID = layer.getID();
+
 	    tilePath.projection()
 	        .translate([k / 2 - d[0] * 256, k / 2 - d[1] * 256])
 	        .scale(k / 2 / Math.PI);
 
-	    //var choropleth = checkChoropleth(layer, json.features);
-
-	    var paths = svg.selectAll("path")
+	    var paths = svg.selectAll('.'+pathLayerID)
 	        .data(json.features)
 	      	.enter().append("path")
-	      	.attr('class', function(gj) {
-	    		var cls = 'path';
+	      	.attr('class', function(d) {
+	    		var cls = 'path' + ' ' + pathLayerID;
+
 	    		layer.getProperties().forEach(function(prop) {
-	    			cls += ' ' + prop + '-' + gj.properties[prop];
+	    			cls += ' ' + prop + '-' + d.properties[prop];
 	    		});
+
 	    		layer.getStyles().forEach(function(style) {
 	    			cls += ' ' + style;
 	    		});
 	    		return cls;
-	    	})/*
-	    	.each(function(d) {
-	    		if (choropleth != false) {
-	    			d3.select(this)
-	    				.style(choropleth.style, choropleth.scale(d.properties[choropleth.attr]));
-	    		}
-	    	})*/
+	    	})
 	        .attr("d", tilePath);
+
+	  	var hover = layer.getHover();
+
+    	if (hover !== false) {
+	    	paths.each(function(d) {
+    			var path = d3.select(this),
+    				tag = 'path-' + d.id;
+
+    			path.classed(tag, true)
+    				.on('mouseover', function(d) {
+	    				d3.selectAll('.'+tag).classed(hover, true);
+			    	})
+			    	.on('mouseout', function(d) {
+	    				d3.selectAll('.'+tag).classed(hover, false);
+			    	});
+	    	});
+	    }
     }
 
-	function _drawTile2(thing, d, tilePath, layer, url) {
-		//console.log(url);
-
-	  	var svg = d3.select(thing);
-
-	  	thing._xhr = d3.json(url, function(error, json) {
-		    //console.log(json);
-		    json = topojson.feature(json, json.objects.vectile);
-		    var k = Math.pow(2, d[2]+8); // size of the world in pixels
-		    //console.log(json);
-
-		    //layer.putCache();
-
-		    tilePath.projection()
-		        .translate([k / 2 - d[0] * 256, k / 2 - d[1] * 256]) // [0°,0°] in pixels
-		        .scale(k / 2 / Math.PI);
-
-		    //var choropleth = checkChoropleth(layer, json.features);
-
-		    var paths = svg.selectAll("path")
-		        .data(json.features)
-		      	.enter().append("path")
-		      	.attr('class', function(gj) {
-		    		var cls = 'path';
-		    		layer.getProperties().forEach(function(prop) {
-		    			cls += ' ' + prop + '-' + gj.properties[prop];
-		    		});
-		    		layer.getStyles().forEach(function(style) {
-		    			cls += ' ' + style;
-		    		});
-		    		return cls;
-		    	})/*
-		    	.each(function(d) {
-		    		if (choropleth != false) {
-		    			d3.select(this)
-		    				.style(choropleth.style, choropleth.scale(d.properties[choropleth.attr]));
-		    		}
-		    	})*/
-		        .attr("d", tilePath);
-	  	});
-
-		function checkChoropleth(layer, features) {
-			return false
-			if (features.length === 0)
-				return
-
-			var choros = layer.getChoros();
-			if (choros === false)
-				return false;
-
-		    var	min = features[0].properties[choros.attr],
-	    		max = features[0].properties[choros.attr],
-	    		data = [],
-	    		cur;
-
-	    	features.forEach(function(f) {
-	    		cur = f.properties[choros.attr];
-	    		data.push(cur);
-	    		if (cur > max)
-	    			max = cur;
-	    		if (cur < min)
-	    			min = cur;
-	    	});
-	    	var choroScale = d3.scale.quantize()
-	    			.domain([min, max])
-	    			.range(choros.range);
-
-	    	var obj = {scale: choroScale, attr: choros.attr};
-	    	obj.style = choros.style || 'fill';
-
-	    	return obj;
-		}
-	}
-
     // map constructor function
-    function _Map(id, options) {//startLoc, startScale, scaleRange) {
-    	var self = this;
+    function _Map(id, options) {
+    	var self = this,
+	    	layers = [],
+    		layerIDs = 0;
 
     	self.IDtag = id;
-
-		var tileLayer = d3.select(self.IDtag).append('g').attr('class', 'layer');
 
     	if (typeof options !== 'undefined') {
 	    	self.startLoc = options.startLoc || [-73.8064, 42.6942]; // defaults to Albany, NY
@@ -327,8 +317,7 @@
 
     	var width = parseInt(d3.select(self.IDtag).style('width')),
     		height = parseInt(d3.select(self.IDtag).style('height')),
-	    	prefix = prefixMatch(["webkit", "ms", "Moz", "O"]),
-	    	layers = [];
+	    	prefix = prefixMatch(["webkit", "ms", "Moz", "O"]);
 
     	var mapTile = d3.geo.tile().size([width, height]);
 
@@ -344,25 +333,23 @@
 		    .scale(projection.scale() * 2 * Math.PI)
 		    .scaleExtent(self.scaleRange)
 		    .translate(projection(self.startLoc).map(function(x) { return -x; }))
-		    .on("zoom", zoomLayers);
+		    .on("zoom", zoomMap);
 
 		var map = d3.select(self.IDtag)
 		    .attr("class", "map")
 		    .style("width", width + "px")
 		    .style("height", height + "px")
 		    .call(zoom)
-		    .on("mousemove", mousemoved);
+		    .on("mousemove", mouseMoved);
+
+		var layersDiv = map.append("div")
+			.attr('id', 'z-index-0')
+		    .attr("class", "layersDiv");
 
 		var info = map.append("div")
 		    .attr("class", "info");
 
-		function zoomLayers() {
-			layers.forEach(function(layer) {
-				zoomed(layer);
-			});
-		}
-
-		function zoomed(layer) {
+		function zoomMap() {
 			var tiles = mapTile
 			    .scale(zoom.scale())
 			    .translate(zoom.translate())();
@@ -371,25 +358,50 @@
 		      	.scale(zoom.scale() / 2 / Math.PI)
 		      	.translate(zoom.translate());
 
-		  	var image = layer.getLayer()
+		  	var layerTiles = layersDiv
 		  		.style(prefix + "transform", matrix3d(tiles.scale, tiles.translate))
 		    	.selectAll(".tile")
 		      	.data(tiles, function(d) { return d; });
 
-		  	image.exit()
-		    	.each(function(d) { layer.abortXHR(this); })//{ this._xhr.abort(); })
+		  	layerTiles.exit()
+		    	.each(function(d) {
+		    		for (i in layers) {
+		    			layers[i].abortXHR(d);
+		    		}
+		    	})
 		    	.remove();
 
-		  	image.enter().append('svg')
+		  	layerTiles.enter().append('svg')
 		    	.attr("class", 'tile')
 		    	.style("left", function(d) { return d[0] * 256 + "px"; })
 		    	.style("top", function(d) { return d[1] * 256 + "px"; })
 		    	.each(function(d) {
-		    		layer.drawTile(this, d, tilePath);
+		    		for (i in layers) {
+		    			layers[i].drawTile(this, d);
+		    		}
 		    	});
 		}
 
-		function mousemoved() {
+		function _drawLayer(layerObj) {
+			var tiles = mapTile
+			    .scale(zoom.scale())
+			    .translate(zoom.translate())();
+
+		  	var layerTiles = layersDiv
+		  		.style(prefix + "transform", matrix3d(tiles.scale, tiles.translate))
+		    	.selectAll(".tile")
+		      	.data(tiles, function(d) { return d; });
+
+		  	layerTiles.each(function(d) {
+		    		layerObj.drawTile(this, d);
+		    	});
+		}
+
+		self.drawLayer = function(layerObj) {
+			_drawLayer(layerObj);
+		}
+
+		function mouseMoved() {
 		  	info.text(formatLocation(projection.invert(d3.mouse(this)), zoom.scale()));
 		}
 
@@ -402,8 +414,20 @@
 		self.addLayer = function(layer) {
 			if (typeof layer !== 'undefined') {
 				layers.push(layer);
-				layer.setLayer(d3.select(self.IDtag).append('g').attr('class', 'layer'));
-				zoomed(layer);
+
+				layers.sort(function(a, b) {
+					return a.getZIndex() - b.getZIndex();
+				});
+
+				layer.setID('layer-'+(layerIDs++));
+				layer.setTilePath(tilePath);
+				layer.setMap(self);
+
+				if (layers.length === 1) {
+					zoomMap();
+				} else {
+					_drawLayer(layer);
+				}
 			} else {
 				throw new ObjectException("No Layer Object argument");
 			}
@@ -413,6 +437,7 @@
 			var index = layers.indexOf(layer);
 			if (index !== -1) {
 				layers.splice(index, 1);
+				d3.selectAll('.'+layer.getID()).remove();
 			}
 		}
     }
@@ -431,15 +456,15 @@
 	  	return "";
 	}
 
-	avl.TileLayer = function(url, options) {//func, classes) {
+	avl.TileLayer = function(url, options) {
 		if (typeof url !== 'undefined') {
-			return new _TileLayer(url, options);//func, classes);
+			return new _TileLayer(url, options);
 		} else {
 			throw new ObjectException("You must specify a map URL");
 		}
 	}
 
-	avl.Map = function(id, options) {//startLoc, startScale, scaleRange) {
+	avl.Map = function(id, options) {
 		if (typeof id !== 'undefined') {
 			if (document.getElementById(id) === null) {
 		    	var width = Math.max(960, window.innerWidth),
@@ -450,7 +475,7 @@
 				    .style("width", width + "px")
 				    .style("height", height + "px");
 			}
-			return new _Map(id, options);//startLoc, startScale, scaleRange);
+			return new _Map(id, options);
 		} else {
 			throw new ObjectException("You must specify a map ID");
 		}
