b9be2e389784500911afd6201d73780e20ffdceb
Eric Conklin <eric.s.conklin@gmail.com>
2014-07-09 14:55:55 -0400

    stuff

--- AVAILmap.js
+++ AVAILmap.js
@@ -276,7 +276,6 @@
                     if (dataDecoder) {
                         json = dataDecoder(json);
                     }
-
                     cache.data(id, json);
                     drawFunc(SVG, d, tilePath, self, json);
                 });
@@ -307,7 +306,7 @@
         url = url.replace(/{z}/, d[2]);
         url = url.replace(/{x}/, d[0]);
         url = url.replace(/{y}/, d[1]);
-		
+
         return url;
     }
 
@@ -665,6 +664,10 @@
 
         self.addControl = function(type, position) {
 			position = _getPosition(position);
+
+            if (position === null) {
+                return;
+            }
 			
             if (type === 'info' && !controls.info) {
                 controls.info = new _InfoControl(map, projection, zoom, position);
@@ -679,19 +682,24 @@
                 controls.marker = new _MarkerControl(mapObj, map, projection, zoom, position);
             }
         }
+        
         function _getPosition(pos) {
             pos = pos || 'top-right';
-            
-            var index = allPositions.indexOf(pos);
-            
-            while (positionsUsed[pos]) {
-                index = (index + 1) % allPositions.length;
-                pos = allPositions[index];
-            }
-            positionsUsed[pos] = true;
 
-            return pos;
+            var index = allPositions.indexOf(pos);
+
+            for (var x = 0; x < 4; x++) {
+                if (positionsUsed[pos]) {
+                    index = (index + 1) % allPositions.length;
+                    pos = allPositions[index];
+                } else {
+                    positionsUsed[pos] = true;
+                    return pos;
+                }
+            }
+            return null;
         }
+
         self.customControl = function(options) {
             var position = 'top-right',
                 name = 'Custom Control ' + customControlsIDs++,
@@ -885,6 +893,61 @@
         }
     }
 
+    AVLtile = function() {
+        var size = [960, 500],
+            scale = 256,
+            translate = [size[0] / 2, size[1] / 2],
+            zoomDelta = 0;
+
+        function tile() {
+            var z = Math.max(Math.log(scale) / Math.LN2 - 8, 0),
+                z0 = Math.round(z + zoomDelta),
+                k = Math.pow(2, z - z0 + 8),
+                origin = [(translate[0] - scale / 2) / k, (translate[1] - scale / 2) / k];
+
+            var tiles = [],
+                cols = d3.range(Math.max(0, Math.floor(-origin[0])), Math.max(0, Math.ceil(size[0] / k - origin[0]))),
+                rows = d3.range(Math.max(0, Math.floor(-origin[1])), Math.max(0, Math.ceil(size[1] / k - origin[1])));
+
+            rows.forEach(function(y) {
+                cols.forEach(function(x) {
+                    tiles.push([x, y, z0]);
+                });
+            });
+
+            tiles.translate = origin;
+            tiles.scale = k;
+
+            return tiles;
+        }
+
+        tile.size = function(_) {
+            if (!arguments.length) return size;
+            size = _;
+            return tile;
+        };
+
+        tile.scale = function(_) {
+            if (!arguments.length) return scale;
+            scale = _;
+            return tile;
+        };
+
+        tile.translate = function(_) {
+            if (!arguments.length) return translate;
+            translate = _;
+            return tile;
+        };
+
+        tile.zoomDelta = function(_) {
+            if (!arguments.length) return zoomDelta;
+            zoomDelta = +_;
+            return tile;
+        };
+
+          return tile;
+    };
+
     // map constructor function
     function _Map(IDtag, options, cntrls) {
         var self = this,
@@ -921,7 +984,7 @@
             height = parseInt(d3.select(IDtag).style('height')),
             prefix = prefixMatch(["webkit", "ms", "Moz", "O"]);
 
-        var mapTile = d3.geo.tile().size([width, height]);
+        var mapTile = AVLtile().size([width, height]);
 
         var projection  = d3.geo.mercator()
             .scale(startZoom / 2 / Math.PI)
@@ -1008,7 +1071,6 @@
                 markers[i].update(currentZoom);
             }
         }
-        self.zoomMap();
 
         self.drawLayer = function(layerObj) {
             var tiles = mapTile
@@ -1077,7 +1139,11 @@
                         .attr('id', 'vector-layer')
                         .attr("class", "layersDiv");
                 }
-                self.drawLayer(layer);
+                if (layers.length == 1) {
+                    self.zoomMap();
+                } else {
+                    self.drawLayer(layer);
+                }
             } else {
                 throw new AVAILmapException("No Layer Object argument");
             }
@@ -1100,7 +1166,11 @@
                     .attr("class", "layersDiv")
                     .style('z-index', -5);
 
-                self.drawRasterLayer();
+                if (layers.length == 0) {
+                    self.zoomMap();
+                } else {
+                    self.drawRasterLayer();
+                }
             } else {
                 throw new AVAILmapException("No Layer Object argument");
             }
